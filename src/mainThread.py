eb6793605d9cb3fefaff0facbdcee01f00f9337c
talwat <talrac1020@gmail.com>
2021-12-03 10:01:41 +0100

    Added the ability to use the program without the gui.

--- src/mainThread.py
+++ src/mainThread.py
@@ -1,6 +1,15 @@
 import ui
 import version
-    
+import sys
+import overlay
+
+args = sys.argv
 print("SkinModPy {}\n".format(version.version))
 
-ui.main()
+if(".py" in args[0]):
+    args.pop(0)
+
+if(len(args) <= 1):
+    ui.main()
+else:
+    overlay.main(args[0], args[1])
